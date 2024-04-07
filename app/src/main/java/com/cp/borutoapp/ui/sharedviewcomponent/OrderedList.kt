b00c635efe034e142405c591223de7b2c6412ef1
CPeeee <cphan1363@gmail.com>
2024-04-06 23:51:13 -0700

    Create OrderedList SharedViewComponent

---
+++ app/src/main/java/com/cp/borutoapp/ui/sharedviewcomponent/OrderedList.kt
@@ -0,0 +1,57 @@
+package com.cp.borutoapp.ui.sharedviewcomponent
+
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.cp.borutoapp.ui.theme.SMALL_PADDING
+import com.cp.borutoapp.ui.theme.titleColor
+
+@Composable
+fun OrderedList(
+    title: String,
+    items: List<String>,
+    textColor: Color
+) {
+
+    Column{
+        Text(
+            modifier = Modifier.padding(bottom = SMALL_PADDING),
+            text = title,
+            color = textColor,
+            fontSize = MaterialTheme.typography.titleSmall.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+        items.forEachIndexed { index, item ->
+            Text(
+                modifier = Modifier.alpha(ContentAlpha.medium),
+                text = "${index + 1}. $item",
+                color = textColor,
+                fontSize = MaterialTheme.typography.bodySmall.fontSize
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrderedListPreview() {
+    OrderedList(title = "Family", items = listOf("Minato", "Kushina"), textColor = MaterialTheme.colorScheme.titleColor)
+}
+
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES, backgroundColor = 0xFF000000)
+@Composable
+fun OrderedListPreviewInDarkMode() {
+    OrderedList(title = "Family", items = listOf("Minato", "Kushina"), textColor = MaterialTheme.colorScheme.titleColor)
+}
