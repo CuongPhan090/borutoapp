96dba9625a9e136744ac58017fd6a9d00d73f6a1
CPeeee <cphan1363@gmail.com>
2024-04-13 23:43:50 -0700

    Update baseUrl

--- app/src/main/java/com/cp/borutoapp/util/Constant.kt
+++ app/src/main/java/com/cp/borutoapp/util/Constant.kt
@@ -11,5 +11,5 @@ object Constant {
 
     const val DATA_STORE_OPERATIONS = "data_store_operations"
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "https://borutoserver-rcvb.onrender.com/"
 }
